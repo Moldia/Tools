a4f50e2c97c4c0a9f68e2259576f91b2985021cb
Christoffer Mattsson Langseth <christoffer.langseth@scilifelab.se>
2019-12-06 15:19:35 +0100

    Update averageIntensity.m

--- Visualization tools/SignalNoiseRation & Intensity/averageIntensity.m
+++ Visualization tools/SignalNoiseRation & Intensity/averageIntensity.m
@@ -18,58 +18,92 @@ cycleNumber = 3; % enter the cycle number
 
 for  s = 2:5 % four channels 
     for k = ["SBH","SBL"] % two sequencing chemistries 
-        Mean = [];
-        Std = []; 
+       Mean = [];
+       Std = [];
+       Tabledata = [];
+       Mean_SBH = [];
+       Mean_SBL = [];
+       Std_SBH = []; 
+       Std_SBL = []; 
         for r = ["ROI1", "ROI2", "ROI3"] % three regions of interest
-                tabledata = readtable(strcat('/Volumes/SBHvsSBL/imagesAndCSVFilesFor3ROI/', 'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber), '/', (k), '_', 'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber), '_', (k), '_', (r), '/', (k), '_', 'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber), '_', (k), '_', (r), '_c', num2str(s), '_ORG.csv'));
-
-                REMOVEDOUTLIERS = rmoutliers(tabledata, 'grubbs');
-                REMOVEDOUTLIERS = table2array(REMOVEDOUTLIERS);
-                IntensityDataMean = mean(REMOVEDOUTLIERS, 1); 
-                IntensityDataStd = std(REMOVEDOUTLIERS, 1);
+                tabledata = readtable(strcat('F:\imagesAndCSVFilesFor3ROI\', 'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber), '\', (k), '_', 'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber), '_', (k), '_', (r), '\', (k), '_', 'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber), '_', (k), '_', (r), '_c', num2str(s), '_ORG.csv'));
+                
+%                 REMOVEDOUTLIERS = rmoutliers(tabledata, 'grubbs');
+%                 REMOVEDOUTLIERS = table2array(REMOVEDOUTLIERS);
+%                 IntensityDataMean = mean(REMOVEDOUTLIERS, 1); 
+%                 IntensityDataStd = std(REMOVEDOUTLIERS, 1);
                 
                 %IntensityData = cat(1, IntensityData, middlePixelIntensity);
-                IntensityDataMean = cat(1, Mean, IntensityDataMean);
-
+%                 IntensityDataMean = cat(1, Mean, IntensityDataMean);
+% 
+%                 
+%                 IntensityDataStd = cat(1, Std, IntensityDataStd);
                 
-                IntensityDataStd = cat(1, Std, IntensityDataStd);
-
+                if r == "ROI1"
+                        tabledataROI1 = tabledata;
+                elseif r == "ROI2" 
+                        tabledataROI2 = tabledata;
+                else 
+                        tabledataROI3 = tabledata;
+                end
+ 
         end
         
-     
         if k == "SBH" 
-            % intensity mean values
-            IntensityDataMeanSBH = IntensityDataMean; 
-            
-            % intensity standard deviation values
-            IntensityDataStdSBH = IntensityDataStd;
-
+                allDataSBH = [tabledataROI1; tabledataROI2; tabledataROI3];
+                allDataSBH_rmoutliers = rmoutliers(allDataSBH); 
+                meanSBH = mean(table2array(allDataSBH_rmoutliers), 1);
+                stdSBH = std(table2array(allDataSBH_rmoutliers), 1);
         else 
-            % intensity mean values
-            IntensityDataMeanSBL = IntensityDataMean; 
-            
-            % intensity standard deviation values
-            IntensityDataStdSBL = IntensityDataStd;
-
+                allDataSBL = [tabledataROI1; tabledataROI2; tabledataROI3];
+                allDataSBL_rmoutliers = rmoutliers(allDataSBL);
+                meanSBL = mean(table2array(allDataSBL_rmoutliers), 1);
+                stdSBL = std(table2array(allDataSBL_rmoutliers), 1);
+ 
         end
     
     end 
     
-    
+    if s == 2
+        allSBH_c2 = allDataSBH_rmoutliers;
+        allSBL_c2 = allDataSBL_rmoutliers;
+    elseif s == 3
+        allSBH_c3 = allDataSBH_rmoutliers;
+        allSBL_c3 = allDataSBL_rmoutliers;
+    elseif s == 4
+        allSBH_c4 = allDataSBH_rmoutliers;
+        allSBL_c4 = allDataSBL_rmoutliers;
+    else
+        allSBH_c5 = allDataSBH_rmoutliers;
+        allSBL_c5 = allDataSBL_rmoutliers;
+    end    
     co = [0.0 0.0 1.0;...
           1.0 0.0 0.0];
     set(groot,'defaultAxesColorOrder',co)
-   
-    sgt = sgtitle(['Average intensity ',  'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber)]);
-    sgt.FontSize = 30;
+    
+    [h,p,ci,stats] = ttest2(meanSBH,meanSBL);
+ 
+    sgt = sgtitle({['Average intensity over RCPs: ',  'Round', num2str(roundNumber), 'Cycle', num2str(cycleNumber)]; ''});
+    sgt.FontSize = 50;
     subplot(2,2, s-1);
-    errorbar(IntensityDataMeanSBH, IntensityDataStdSBH, 'MarkerSize',10,'MarkerEdgeColor','red','MarkerFaceColor','red')
+    errorbar(meanSBH, stdSBH, 'MarkerSize',10,'MarkerEdgeColor','red','MarkerFaceColor','red')
     hold on 
-    errorbar(IntensityDataMeanSBL, IntensityDataStdSBL, 'MarkerSize',10,'MarkerEdgeColor','red','MarkerFaceColor','red')
+    errorbar(meanSBL, stdSBL, 'MarkerSize',10,'MarkerEdgeColor','red','MarkerFaceColor','red')
+    
     tit = title(['Channel  ', num2str(s)]);
-    tit.FontSize = 25;
+    tit.FontSize = 20 ;
     ylab = ylabel('Intensity');
     ylab.FontSize = 20; 
-    ylim([0, 5000]);
+    
+    xlab = xlabel({'Pixel number'; strcat('p value:', '    ', num2str(round(p, 2)))});
+    xlab.FontSize = 20;
+    
+    ylim([0,    6000]);
+    xlim([1,21]);
+    
+    lgd = legend ('SBH', 'SBL');
+    lgd.FontSize = 15;         
+
+
     
 end
